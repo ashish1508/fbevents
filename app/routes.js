48059262a29d0957ceb2dba63750bcd2bb8ae404
ashish1508 <belide.ashish@gmail.com>
2018-02-14 07:32:49 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -82,7 +82,7 @@ module.exports = function(app){
 
     app.get('/home',function(req,res){
 
-    	//res.render('home');
+    	
        
         res.sendFile(path.resolve(__dirname+'/../public/views/home.html'))
       
